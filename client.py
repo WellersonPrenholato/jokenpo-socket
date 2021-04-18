9a4f4fc46dbd97d4959c9f87de110bffa1ac2fc0
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2021-04-18 15:43:50 -0300

    Update client.py

--- client.py
+++ client.py
@@ -10,29 +10,35 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM identific
 
 sock.connect((HOST, PORT)) # Parênteses duplo pq o connect tem apenas um parâmetro (Realiza conexão com o servidor)
 
-opcao1 = int(input("\n* Escolha uma opção:\n 1- Palpite aleatório\n 2- Informar um palpite\n -> Opcao: "))
+while True:
 
-if (opcao1 == 1):
-    mensagemEnvioClient = random.choice(opcoesJogadas)
+    opcao1 = int(input("\n* Escolha uma opção:\n 1- Palpite aleatório\n 2- Informar um palpite\n 0 - Para encerrar.\n -> Opcao: "))
 
-if (opcao1 == 2):
-    opcao2 = int(input("\n* Ecolha o palpite:\n 1- Pedra\n 2- Papel\n 3- Tesoura\n -> Opcao: "))
+    if (opcao1 == 1):
+        mensagemEnvioClient = random.choice(opcoesJogadas)
 
-    if (opcao2 == 1):
-        mensagemEnvioClient = 'Pedra'
-    elif (opcao2 == 2):
-        mensagemEnvioClient = 'Papel'
-    elif (opcao2 == 3):
-        mensagemEnvioClient = 'Tesoura'
+    if (opcao1 == 2):
+        opcao2 = int(input("\n* Ecolha o palpite:\n 1- Pedra\n 2- Papel\n 3- Tesoura\n -> Opcao: "))
+
+        if (opcao2 == 1):
+            mensagemEnvioClient = 'Pedra'
+        elif (opcao2 == 2):
+            mensagemEnvioClient = 'Papel'
+        elif (opcao2 == 3):
+            mensagemEnvioClient = 'Tesoura'
+
+    sock.sendall(str.encode(mensagemEnvioClient)) # Enviar mensagem para o servidor
+
+    print("\nPalpite enviado pelo cliente: ", mensagemEnvioClient)
+
+    # Resposta do servidor
+    data = sock.recv(1024) # Bytes
+
+    print('\nResultado final do jogo: \n', data.decode().upper())
 
 
-# mensagemEnvioClient = random.choice(opcoesJogadas)
-sock.sendall(str.encode(mensagemEnvioClient)) # Enviar mensagem para o servidor
-
-print("\nPalpite enviado pelo cliente: ", mensagemEnvioClient)
-
-# Resposta do servidor
-data = sock.recv(1024) # Bytes
-
-print('\nResultado final do jogo: \n', data.decode().upper())
+    # opcao3 = int(input("\n0 - Para encerrar.\n"))
+    if (opcao1 == 0): 
+        sock.close()
+        break
 
